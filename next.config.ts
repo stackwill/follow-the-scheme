0c9aeadde22c2fa4c57af1a626217950de7d4d2f
Will <williamjhanlon@icloud.com>
2026-05-17 14:46:48 +0100

    Slim production Docker image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "standalone",
   serverExternalPackages: ["@napi-rs/canvas"],
   typedRoutes: true,
 };
